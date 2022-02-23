f104a8aa28f2904d46abdbcfdde296b67a5641d4
EmelyFigueroa03 <98853666+emelyfig@users.noreply.github.com>
2022-02-23 16:05:44 -0600

    Add files via upload

---
+++ 13.Ejercicio17-CicloWhileInfinito.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	
+	
+	int ciclos = 0;
+	
+	
+	
+	while(ciclos < 1000000)
+	{
+		cout << "'";
+		
+		ciclos = ciclos + 1;
+		
+	}
+	
+	
+	
+	return 0;
+}

731ef0916304204d55978db072f1fc62feedec49
EmelyFigueroa03 <98853666+emelyfig@users.noreply.github.com>
2022-02-23 16:01:50 -0600

    Add files via upload

---
+++ 3.Ejercicio03-Variables.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int numeroEntero;
+	numeroEntero = 15;
+	
+	cout << " Entero: " << numeroEntero << endl;
+	
+	numeroEntero = 18;
+	
+	cout << "Entero modificado: " << numeroEntero;
+	
+	return 0;
+	
+}

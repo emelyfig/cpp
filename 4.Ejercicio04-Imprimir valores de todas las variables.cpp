c0ca34aa7a0441dbe3a82147ced34c57a60afee9
EmelyFigueroa03 <98853666+emelyfig@users.noreply.github.com>
2022-02-23 16:02:11 -0600

    Add files via upload

---
+++ 4.Ejercicio04-Imprimir valores de todas las variables.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	bool valorBoolean;
+	valorBoolean = true;
+	
+	int valorEntero = 15;
+	double valorDouble = 20.99;
+	string valorString = "Hola como estan";
+	char valorChar = 'B';
+	
+	cout << " Valor Boolean : " << valorBoolean << endl;
+	cout << " Valor Entero : " << valorEntero   << endl;
+	cout << " Valor double : " << valorDouble   << endl;
+	cout << " Valor String : " << valorString   << endl;
+	cout << " Valor char   : " << valorChar     << endl;
+	
+	return 0;
+}

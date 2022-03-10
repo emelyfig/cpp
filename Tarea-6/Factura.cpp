bde846709dc13771b4bfa2f81dd60299880ddee0
EmelyFigueroa03 <98853666+emelyfig@users.noreply.github.com>
2022-03-10 12:52:45 -0600

    Add files via upload

---
+++ Tarea-6/Factura.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+double subtotal, subImpuesto, total;
+const double impuesto = .15;
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad, double precio)
+{
+    listaProductos = listaProductos + descripcion + '\n';
+    subtotal = subtotal + (cantidad * precio);
+    subImpuesto = subtotal * impuesto;
+    total = subtotal + subImpuesto;
+}
+
+void imprimirFactura()
+{
+    system("cls");
+    cout << "*******" << endl;
+    cout << "FACTURA" << endl;
+    cout << "*******" << endl;
+    cout << endl;
+
+    cout << "Productos:" << endl;
+    cout << listaProductos;
+
+    cout << endl;
+    cout << "Subtotal: " << subtotal << endl;
+    cout << "Impuesto 15%: " << subImpuesto << endl;
+    cout << "Total: " << total << endl;
+    
+    system("pause");
+}

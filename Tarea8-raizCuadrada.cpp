3781dab216d3650987590cfed6c00995abccf77b
EmelyFigueroa03 <98853666+emelyfig@users.noreply.github.com>
2022-03-23 17:21:11 -0600

    Add files via upload

---
+++ Tarea8-raizCuadrada.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main ( int argc, char const *argv[])
+{
+	double numero = 0;
+	double raizCuadrada = 0;
+	
+	cout << " Ingrese un numero: " << endl;
+	cin >> numero;
+	raizCuadrada = sqrt (numero);
+	cout << " La raiz cuadrada de " << numero << " Es: " << raizCuadrada << endl;
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
